d9e9469c17dc67492f4127c0db4acf60e074d1e2
xhw <xhw219@163.com>
2017-04-11 14:53:35 +0800

    增加详情页的toolbar 增加变色状态栏

--- app/src/main/java/com/cloud/college/core/DetailActivity.java
+++ app/src/main/java/com/cloud/college/core/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.cloud.college.core;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.cloud.college.R;
 
@@ -15,7 +18,7 @@ import butterknife.ButterKnife;
  * Created by xiao on 2017/4/9.
  */
 
-public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
+public class DetailActivity extends AppCompatActivity {
 
     @BindView(R.id.detailToolbar) Toolbar toolbar;
 
@@ -24,16 +27,24 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
         ButterKnife.bind(this);
-        //this.setSupportActionBar(toolbar);
         toolbar.inflateMenu(R.menu.detail_toolbar_menu);
-        toolbar.setNavigationOnClickListener(this);
+        //this.setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 int menuItemId = item.getItemId();
                 if (menuItemId == R.id.action_item1) {
 
+                    Toast.makeText(DetailActivity.this, "分享", Toast.LENGTH_SHORT).show();
 
                 }
                 return true;
@@ -41,8 +52,6 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
         });
     }
 
-    @Override
-    public void onClick(View view) {
-        finish();
-    }
+
+
 }
